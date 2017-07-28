cfa1cae6d6ec0603677ccd3a67da549d77bb6b4a
Ephraim Muhia <emuhia@ona.io>
2017-07-28 16:59:08 +0300

    rename app
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- sample/src/main/java/org/smartregister/nativeform/MainActivity.java
+++ sample/src/main/java/org/smartregister/nativeform/MainActivity.java
@@ -63,6 +63,12 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_add) {
+            try {
+                startForm(REQUEST_CODE_GET_JSON, "child_enrollment", null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return super.onOptionsItemSelected(item);
