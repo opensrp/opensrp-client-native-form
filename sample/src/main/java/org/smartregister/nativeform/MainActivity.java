a04749b8d64f004406e4d32d2d33802147aa1a80
Martin Ndegwa <ndegwamartin@gmail.com>
2017-07-28 14:17:58 +0300

    Cleaned up dependencies in gradle config, fixed issue with sample app
    Signed-off-by: Martin Ndegwa <ndegwamartin@gmail.com>

--- sample/src/main/java/org/smartregister/nativeform/MainActivity.java
+++ sample/src/main/java/org/smartregister/nativeform/MainActivity.java
@@ -1,9 +1,7 @@
 package org.smartregister.nativeform;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
