65ff0d4df19b88ccb7375da5f8545db662e2ce49
rkodev <43806892+rkodev@users.noreply.github.com>
2019-02-19 17:17:24 +0300

    Collapsed If

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/JsonFormActivity.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/JsonFormActivity.java
@@ -1722,11 +1722,9 @@ public class JsonFormActivity extends AppCompatActivity implements JsonApi {
                 } else if (child instanceof MaterialSpinner) {
                     MaterialSpinner spinner = (MaterialSpinner) child;
                     spinner.setSelected(false);
-                } else if(child instanceof TextView){
+                } else if(child instanceof TextView && child.getId() == R.id.duration){
                     // clear duration for custom date picker
-                    if(child.getId() == R.id.duration){
-                        ((TextView) child).setText("");
-                    }
+                    ((TextView) child).setText("");
                 }
                 refreshViews(group.getChildAt(id));
             }
