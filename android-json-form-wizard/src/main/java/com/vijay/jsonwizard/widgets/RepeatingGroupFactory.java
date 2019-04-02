e6f3b100e8d367aa99a41f31db0767ae22a51d3b
Vincent Karuri <vincentmkaruri@gmail.com>
2019-04-02 15:49:59 +0300

    Add repeating_group_layout definition as a json array field in the repeating group widget

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/RepeatingGroupFactory.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/RepeatingGroupFactory.java
@@ -10,21 +10,35 @@ import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.CommonListener;
 import com.vijay.jsonwizard.interfaces.FormWidgetFactory;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Vincent Karuri
  */
 public class RepeatingGroupFactory implements FormWidgetFactory {
+
+    private String REPEATING_GROUP_LAYOUT = "repeating_group_layout";
+    public static Map<Integer, JSONArray> repeatingGroupLayouts = new HashMap<>();
+
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener, boolean popup) throws Exception {
         List<View> views = new ArrayList<>(1);
+
         LinearLayout rootLayout = (LinearLayout) LayoutInflater.from(context).inflate(getLayout(), null);
-        rootLayout.setId(View.generateViewId());
+
+        int rootLayoutId = View.generateViewId();
+        rootLayout.setId(rootLayoutId);
         views.add(rootLayout);
+
+        JSONArray repeatingGroupLayout = jsonObject.getJSONArray(REPEATING_GROUP_LAYOUT);
+        repeatingGroupLayouts.put(rootLayoutId, repeatingGroupLayout);
+
         return views;
     }
 
