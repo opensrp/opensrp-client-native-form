c33669a8470680fd7a4351306908db8128c5807e
Vincent Karuri <vincentmkaruri@gmail.com>
2019-05-27 13:25:51 +0300

    Fix bug associated with fragment transactions on commit

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/fragments/JsonFormFragment.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/fragments/JsonFormFragment.java
@@ -441,7 +441,7 @@ public class JsonFormFragment extends MvpFragment<JsonFormFragmentPresenter, Jso
         getActivity().getSupportFragmentManager().beginTransaction()
                 .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left,
                         R.anim.exit_to_right).replace(R.id.container, next).addToBackStack(next.getClass().getSimpleName())
-                .commit();
+                .commitAllowingStateLoss(); // use https://stackoverflow.com/a/10261449/9782187
     }
 
     public Menu getMenu() {
