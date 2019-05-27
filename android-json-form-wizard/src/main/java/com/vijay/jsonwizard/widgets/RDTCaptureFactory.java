fe52457e17ba9807af3f248907e4c303e04c9db5
Vincent Karuri <vincentmkaruri@gmail.com>
2019-05-27 17:06:22 +0300

    Write image file path to rdt capture widget value field

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/RDTCaptureFactory.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/RDTCaptureFactory.java
@@ -17,6 +17,7 @@ import com.vijay.jsonwizard.interfaces.FormWidgetFactory;
 import com.vijay.jsonwizard.interfaces.JsonApi;
 import com.vijay.jsonwizard.interfaces.OnActivityResultListener;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -25,6 +26,8 @@ import java.util.List;
 import edu.washington.cs.ubicomplab.rdt_reader.activity.RDTCaptureActivity;
 
 import static android.app.Activity.RESULT_OK;
+import static com.vijay.jsonwizard.constants.JsonFormConstants.VALUE;
+import static edu.washington.cs.ubicomplab.rdt_reader.Constants.SAVED_IMAGE_FILE_PATH;
 
 /**
  * Created by Vincent Karuri on 24/05/2019
@@ -32,6 +35,7 @@ import static android.app.Activity.RESULT_OK;
 public class RDTCaptureFactory implements FormWidgetFactory {
 
     private static final String TAG = RDTCaptureFactory.class.getName();
+    private View rootLayout;
 
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener, boolean popup) throws Exception {
@@ -43,10 +47,12 @@ public class RDTCaptureFactory implements FormWidgetFactory {
                 .withListener(listener)
                 .withPopup(popup);
 
+        rootLayout = LayoutInflater.from(context).inflate(getLayout(), null);
+
+        addWidgetTags(jsonObject);
         setUpRDTCaptureActivity(widgetArgs);
         launchRDTCaptureActivity((Activity) context);
 
-        LinearLayout rootLayout = (LinearLayout) LayoutInflater.from(context).inflate(getLayout(), null);
         List<View> views = new ArrayList<>(1);
         views.add(rootLayout);
 
@@ -58,26 +64,59 @@ public class RDTCaptureFactory implements FormWidgetFactory {
         return getViewsFromJson(stepName, context, formFragment, jsonObject, listener, false);
     }
 
+    private void addWidgetTags(JSONObject jsonObject) throws JSONException {
+        String openMrsEntityParent = jsonObject.getString(JsonFormConstants.OPENMRS_ENTITY_PARENT);
+        String openMrsEntity = jsonObject.getString(JsonFormConstants.OPENMRS_ENTITY);
+        String openMrsEntityId = jsonObject.getString(JsonFormConstants.OPENMRS_ENTITY_ID);
+        String key = jsonObject.getString(JsonFormConstants.KEY);
+
+        rootLayout.setTag(R.id.key, key);
+        rootLayout.setTag(R.id.openmrs_entity_parent, openMrsEntityParent);
+        rootLayout.setTag(R.id.openmrs_entity, openMrsEntity);
+        rootLayout.setTag(R.id.openmrs_entity_id, openMrsEntityId);
+    }
+
+
+    private OnActivityResultListener createOnActivityResultListener(final WidgetArgs widgetArgs) {
+
+         OnActivityResultListener resultListener =  new OnActivityResultListener() {
+
+                @Override
+                public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                    if (requestCode == JsonFormConstants.RDT_CAPTURE_CODE && resultCode == RESULT_OK) {
+                        if (data != null) {
+                            try {
+                                String imageFilePath = data.getExtras().getString(SAVED_IMAGE_FILE_PATH);
+                                widgetArgs.getJsonObject().put(VALUE, imageFilePath);
+                                // write image path as widget value
+                                String key = (String) rootLayout.getTag(R.id.key);
+                                String openMrsEntityParent = (String) rootLayout.getTag(R.id.openmrs_entity_parent);
+                                String openMrsEntity = (String) rootLayout.getTag(R.id.openmrs_entity);
+                                String openMrsEntityId = (String) rootLayout.getTag(R.id.openmrs_entity_id);
+                                final JsonApi jsonApi = (JsonApi) widgetArgs.getContext();
+                                jsonApi.writeValue(widgetArgs.getStepName(), key, imageFilePath, openMrsEntityParent,
+                                        openMrsEntity, openMrsEntityId, widgetArgs.isPopup());
+                            } catch (JSONException e) {
+                                Log.e(TAG, e.getStackTrace().toString());
+                            }
+                        } else {
+                            Log.i(TAG, "No result data for RDT capture!");
+                        }
+                        if (!widgetArgs.getFormFragment().next()) {
+                            widgetArgs.getFormFragment().save(true);
+                        }
+                    }
+                }
+        };
+
+        return resultListener;
+    }
+
     public void setUpRDTCaptureActivity(final WidgetArgs widgetArgs) {
         Context context = widgetArgs.getContext();
         if (context instanceof JsonApi) {
-            JsonApi jsonApi = (JsonApi) context;
-            jsonApi.addOnActivityResultListener(JsonFormConstants.RDT_CAPTURE_CODE ,
-                    new OnActivityResultListener() {
-                        @Override
-                        public void onActivityResult(int requestCode, int resultCode, Intent data) {
-                            if (requestCode == JsonFormConstants.RDT_CAPTURE_CODE && resultCode == RESULT_OK) {
-                                if (data != null) {
-                                   // todo: do something
-                                } else {
-                                    Log.i(TAG, "No result data for RDT capture!");
-                                }
-                                if (!widgetArgs.getFormFragment().next()) {
-                                    widgetArgs.getFormFragment().save(true);
-                                }
-                            }
-                        }
-                    });
+            final JsonApi jsonApi = (JsonApi) context;
+            jsonApi.addOnActivityResultListener(JsonFormConstants.RDT_CAPTURE_CODE , createOnActivityResultListener(widgetArgs));
         }
     }
 
