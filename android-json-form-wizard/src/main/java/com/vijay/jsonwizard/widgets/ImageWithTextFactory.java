d3d345c23d624a01af8effb131be0e430c165401
Allan <a.onchuru@gmail.com>
2019-06-17 00:25:46 +0300

    WIP : Display text and image in wizard form

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/ImageWithTextFactory.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/ImageWithTextFactory.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -18,19 +19,21 @@ import com.vijay.jsonwizard.interfaces.FormWidgetFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ImageWithTextFactory implements FormWidgetFactory {
 
+    private static String TAG = ImageWithTextFactory.class.getCanonicalName();
     private View rootLayout;
 
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener, boolean popup) throws Exception {
         List<View> views = new ArrayList<>(1);
         rootLayout = LayoutInflater.from(context).inflate(getLayout(), null);
-        setWidgetTags(jsonObject);
+        setWidgetTags(jsonObject, context);
         views.add(rootLayout);
         return views;
     }
@@ -44,25 +47,27 @@ public class ImageWithTextFactory implements FormWidgetFactory {
         return R.layout.native_form_text_and_image;
     }
 
-    private void setWidgetTags(JSONObject jsonObject) throws JSONException {
+    private void setWidgetTags(JSONObject jsonObject, Context context) throws JSONException {
         String key = jsonObject.getString(JsonFormConstants.KEY);
         String openMrsEntityParent = jsonObject.getString(JsonFormConstants.OPENMRS_ENTITY_PARENT);
         String openMrsEntity = jsonObject.getString(JsonFormConstants.OPENMRS_ENTITY);
         String openMrsEntityId = jsonObject.getString(JsonFormConstants.OPENMRS_ENTITY_ID);
         String descriptionText = jsonObject.getString(JsonFormConstants.TEXT);
-        String imagePath = jsonObject.getString(JsonFormConstants.IMAGE_FILE_PATH);
+        String imageFile = jsonObject.getString(JsonFormConstants.IMAGE_FILE);
 
         if (!TextUtils.isEmpty(descriptionText)) {
             TextView descriptionTextView = rootLayout.findViewById(R.id.text);
             descriptionTextView.setText(descriptionText);
         }
 
-        if (!TextUtils.isEmpty(imagePath)) {
+        if (!TextUtils.isEmpty(imageFile)) {
             ImageView imageView = rootLayout.findViewById(R.id.image);
-            File imageFile = new File(imagePath);
-            if (imageFile.exists()) {
-                Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            try {
+                InputStream is = context.getAssets().open(imageFile);
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
                 imageView.setImageBitmap(bitmap);
+            } catch (IOException ioe) {
+                Log.e(TAG, ioe.toString());
             }
         }
 
