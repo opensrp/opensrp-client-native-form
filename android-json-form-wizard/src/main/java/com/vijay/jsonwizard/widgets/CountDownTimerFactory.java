4e291e86056069235e0a4b208763e350ef90f3a2
Allan O <a.onchuru@gmail.com>
2019-06-28 11:49:15 +0300

    Show countdown timer progress and ring alarm when countdown is done

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/CountDownTimerFactory.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/CountDownTimerFactory.java
@@ -1,46 +1,55 @@
 package com.vijay.jsonwizard.widgets;
 
 import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
 
+import com.emredavarci.circleprogressbar.CircleProgressBar;
 import com.vijay.jsonwizard.R;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.CommonListener;
 import com.vijay.jsonwizard.interfaces.FormWidgetFactory;
-import com.vijay.jsonwizard.utils.FormUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class CountDownTimerFactory implements FormWidgetFactory {
 
     private View rootLayout;
-    private TextView countDownTextView;
+    private CircleProgressBar progressBar;
+    private int elapsedCount = 0;
+    private int progressBarMaxValue = 100;
 
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener, boolean popup) throws Exception {
         List<View> views = new ArrayList<>(1);
         rootLayout = LayoutInflater.from(context).inflate(getLayout(), null);
-        countDownTextView = rootLayout.findViewById(R.id.countDown);
+        progressBar = rootLayout.findViewById(R.id.progressBar);
+        progressBar.setStrokeWidthDimension(20);
+        // TextView timerText = rootLayout.findViewById(R.id.text);
 
         setWidgetTags(jsonObject);
         formatWidget(jsonObject, context);
 
         String secondsString = jsonObject.optString(JsonFormConstants.COUNTDOWN_TIMER_SECONDS);
         String interValString = jsonObject.optString(JsonFormConstants.COUNTDOWN_INTERVAL);
-        long seconds = (secondsString.isEmpty()) ? 0 : Long.parseLong(secondsString);
+        long seconds = (secondsString.isEmpty()) ? 0 : Long.parseLong(secondsString) * 1000;
         long defaultCountdownInterval = 1000;
         long interval = (interValString.isEmpty()) ? defaultCountdownInterval : Long.parseLong(interValString);
+        progressBar.setMaxValue(progressBarMaxValue);
+        startCountDown(seconds, interval, context);
         views.add(rootLayout);
-        startCountDown(seconds, interval, countDownTextView);
         return views;
     }
 
@@ -67,7 +76,7 @@ public class CountDownTimerFactory implements FormWidgetFactory {
         if (textSize.isEmpty()) {
             textSize = defaultTimerTextSize;
         }
-        countDownTextView.setTextSize((FormUtils.getValueFromSpOrDpOrPx(textSize, context)));
+        // countDownView.setTextSize((FormUtils.getValueFromSpOrDpOrPx(textSize, context)));
     }
 
     private int getLayout() {
@@ -79,19 +88,38 @@ public class CountDownTimerFactory implements FormWidgetFactory {
      *
      * @param seconds           The time count down to
      * @param countdownInterval The intervals for running the countdown
-     * @param tv                The TextView to show the countdown
      */
-    private void startCountDown(long seconds, long countdownInterval, final TextView tv) {
+    private void startCountDown(final long seconds, long countdownInterval, final Context context) {
         new CountDownTimer(seconds, countdownInterval) {
             @Override
             public void onTick(long millisUntilFinished) {
-                tv.setText(String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished), TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)));
+                elapsedCount++;
+                progressBar.setText(getFormattedTimeText(millisUntilFinished));
+                int progress = (int) (elapsedCount * 100 / (seconds / 1000));
+                progressBar.setProgress(progress);
             }
 
             @Override
             public void onFinish() {
-                // TODO :: Ring the alarm!!
+                elapsedCount = 0;
+                progressBar.setText(getFormattedTimeText(seconds));
+                progressBar.setProgress(progressBarMaxValue);
+                ringAlarm(context);
             }
         }.start();
+    }
+
+    private void ringAlarm(Context context) {
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone alarmTone = (alert == null) ? RingtoneManager.getRingtone(context, Settings.System.DEFAULT_RINGTONE_URI) : RingtoneManager.getRingtone(context, alert);
+        if (!alarmTone.isPlaying()) {
+            alarmTone.play();
+        }
+    }
+
+
+    private String getFormattedTimeText(long timeValue) {
+        return String.format(Locale.getDefault(), "%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(timeValue), TimeUnit.MILLISECONDS.toSeconds(timeValue));
     }
 }
