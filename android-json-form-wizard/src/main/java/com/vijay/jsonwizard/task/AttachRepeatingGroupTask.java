1d9a795333ce27d89a46392b2a18cc546924b722
Manu Tarus <manutarus@gmail.com>
2020-02-15 09:26:50 +0300

    remove unused imports reported by codacy

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/task/AttachRepeatingGroupTask.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/task/AttachRepeatingGroupTask.java
@@ -26,7 +26,6 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
