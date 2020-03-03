783fc2a1717afb5b087c80e1382c66facdbf1f18
Samuel Githengi <samuel.githengi@gmail.com>
2020-03-03 11:29:35 +0300

    Unit test add form elements

---
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/presenters/JsonFormFragmentPresenterRoboElectricTest.java
@@ -0,0 +1,98 @@
+package com.vijay.jsonwizard.presenters;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.vijay.jsonwizard.BaseTest;
+import com.vijay.jsonwizard.TestConstants;
+import com.vijay.jsonwizard.activities.JsonFormActivity;
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.fragments.JsonFormFragment;
+import com.vijay.jsonwizard.interactors.JsonFormInteractor;
+import com.vijay.jsonwizard.interfaces.CommonListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
+import org.robolectric.RuntimeEnvironment;
+
+import java.lang.ref.WeakReference;
+import java.util.Collections;
+import java.util.List;
+
+import static com.vijay.jsonwizard.constants.JsonFormConstants.STEP1;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by samuelgithengi on 3/3/20.
+ */
+public class JsonFormFragmentPresenterRoboElectricTest extends BaseTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private JsonFormFragment formFragment;
+
+    @Mock
+    private JsonFormInteractor jsonFormInteractor;
+
+    private JsonFormFragmentPresenter presenter;
+
+    @Mock
+    private JsonFormActivity jsonFormActivity;
+
+    @Captor
+    private ArgumentCaptor<JSONObject> jsonArgumentCaptor;
+
+    private static JSONObject jsonForm;
+
+    private View textView;
+
+    private Context context = RuntimeEnvironment.application;
+
+    @BeforeClass
+    public static void initForm() throws JSONException {
+        jsonForm = new JSONObject(TestConstants.PAOT_TEST_FORM);
+    }
+
+    @Before
+    public void setUp() {
+        when(formFragment.getJsonApi()).thenReturn(jsonFormActivity);
+        presenter = new JsonFormFragmentPresenter(formFragment, jsonFormInteractor);
+        Whitebox.setInternalState(presenter, "viewRef", new WeakReference<>(formFragment));
+        textView = new TextView(context);
+    }
+
+    @Test
+    public void testAddFormElements() throws JSONException {
+        Bundle bundle = new Bundle();
+        bundle.putString(JsonFormConstants.JSON_FORM_KEY.STEPNAME, STEP1);
+        when(formFragment.getArguments()).thenReturn(bundle);
+        when(formFragment.getStep(STEP1)).thenReturn(jsonForm.getJSONObject(STEP1));
+        List<View> views = Collections.singletonList(textView);
+        when(jsonFormInteractor.fetchFormElements(anyString(), any(JsonFormFragment.class), any(JSONObject.class), isNull(CommonListener.class), anyBoolean())).thenReturn(views);
+        presenter.addFormElements();
+        verify(jsonFormInteractor).fetchFormElements(eq(STEP1), eq(formFragment), jsonArgumentCaptor.capture(), isNull(CommonListener.class), eq(false));
+        assertEquals(jsonForm.getJSONObject(STEP1).toString(), jsonArgumentCaptor.getValue().toString());
+        verify(formFragment).addFormElements(views);
+    }
+}
