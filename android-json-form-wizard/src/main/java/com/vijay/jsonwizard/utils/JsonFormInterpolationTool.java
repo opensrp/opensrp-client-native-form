1ecd854acef14b136a6abf144c7f88c4d378c118
Vincent Karuri <vincentmkaruri@gmail.com>
2020-03-12 16:34:50 +0300

    Add JsonFormInterpolationTool

---
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/utils/JsonFormInterpolationTool.java
@@ -0,0 +1,11 @@
+package com.vijay.jsonwizard.utils;
+
+/**
+ * Created by Vincent Karuri on 12/03/2020
+ */
+public class JsonFormInterpolationTool {
+
+    public static void main(String[] args) {
+        System.out.println("Performing form interpolation...");
+    }
+}
