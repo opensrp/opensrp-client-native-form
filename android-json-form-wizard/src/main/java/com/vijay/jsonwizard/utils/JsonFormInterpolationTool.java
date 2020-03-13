b5001798fc42504dbe2336f1eaa848a658397631
Vincent Karuri <vincentmkaruri@gmail.com>
2020-03-13 14:37:11 +0300

    Move to using Gson to avoid stub android dependencies i.e. org.json

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/utils/JsonFormInterpolationTool.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/utils/JsonFormInterpolationTool.java
@@ -1,9 +1,12 @@
 package com.vijay.jsonwizard.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.vijay.jsonwizard.interactors.JsonFormInteractor;
 
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.util.Map;
 
 import timber.log.Timber;
 
@@ -18,16 +21,29 @@ public class JsonFormInterpolationTool {
         try {
             String fileToTranslate = System.getenv("FILE_TO_TRANSLATE");
             printToSystemOut("Interpolating form at path: " + fileToTranslate + " ...\n");
-            printToSystemOut("List of translatable widget fields:\n");
-            for (String str : JsonFormInteractor.getInstance().getDefaultTranslatableWidgetFields()) {
-                printToSystemOut(str);
-            }
 
             String form = getFileContentsAsString(fileToTranslate);
+
             printToSystemOut("\nForm before interpolation:\n");
             printToSystemOut(form);
+
+            performInterpolation(stringToJson(form));
         } catch (FileNotFoundException e) {
             Timber.e(e);
+        }
+    }
+
+    private static JsonObject stringToJson(String json) {
+        return new Gson().fromJson(json, JsonObject.class);
+    }
+
+    private static void performInterpolation(JsonObject jsonForm) {
+        printToSystemOut("List of translatable widget fields:\n");
+        for (String str : JsonFormInteractor.getInstance().getDefaultTranslatableWidgetFields()) {
+            printToSystemOut(str);
+        }
+        for (Map.Entry<String, JsonElement> entry : jsonForm.entrySet()) {
+            printToSystemOut("The key is: " + entry.getKey() + " and the value is: " + entry.getValue());
         }
     }
 
