bbb35b179691cc28b68838212d3f095060a520f8
bennsimon <botieno@ona.io>
2020-03-18 16:44:49 +0300

    bump up version

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/customviews/TreeViewDialog.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/customviews/TreeViewDialog.java
@@ -190,6 +190,7 @@ public class TreeViewDialog extends Dialog implements TreeNode.TreeNodeClickList
 
     private void setValue(final ArrayList<String> value) {
         this.value = value;
+        extractName();
     }
 
     public ArrayList<String> getName() {
