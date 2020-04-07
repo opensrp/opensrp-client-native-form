b2327e548b5e7a01a8ed75e250e9e33145983deb
rkodev <43806892+rkodev@users.noreply.github.com>
2020-04-07 19:40:12 +0300

    Adds custom config forms

--- sample/src/main/java/org/smartregister/nativeform/interactor/FormTesterInteractor.java
+++ sample/src/main/java/org/smartregister/nativeform/interactor/FormTesterInteractor.java
@@ -14,13 +14,10 @@ import org.smartregister.nativeform.domain.ConfigForm;
 import org.smartregister.nativeform.domain.JsonForm;
 import org.smartregister.nativeform.util.FileReaderUtil;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -171,23 +168,4 @@ public class FormTesterInteractor implements FormTesterContract.Interactor {
         return (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
     }
 
-
-    private String getStringFromFile(File sourceFile) throws Exception {
-        FileInputStream fin = new FileInputStream(sourceFile);
-        String ret = convertStreamToString(fin);
-        //Make sure you close all streams.
-        fin.close();
-        return ret;
-    }
-
-    private String convertStreamToString(InputStream is) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        reader.close();
-        return sb.toString();
-    }
 }
