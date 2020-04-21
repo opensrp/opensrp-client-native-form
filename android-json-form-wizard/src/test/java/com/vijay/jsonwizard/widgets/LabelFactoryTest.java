3a59fbe32bfd2e40bccc724f54646921b9a85b6c
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-04-21 11:55:51 +0300

    :heavy_check_mark: Add tests for the label, gps, hidden & barcode factory

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/LabelFactoryTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/LabelFactoryTest.java
@@ -3,21 +3,24 @@ package com.vijay.jsonwizard.widgets;
 import android.content.res.Resources;
 import android.support.constraint.ConstraintLayout;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.vijay.jsonwizard.BaseTest;
+import com.vijay.jsonwizard.R;
 import com.vijay.jsonwizard.activities.JsonFormActivity;
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.CommonListener;
 import com.vijay.jsonwizard.utils.FormUtils;
+import com.vijay.jsonwizard.views.CustomTextView;
 
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
 
 import java.util.List;
 
@@ -36,10 +39,13 @@ public class LabelFactoryTest extends BaseTest {
     private CommonListener listener;
 
     @Mock
-    private LinearLayout rootLayout;
+    private ConstraintLayout constraintLayout;
 
     @Mock
-    private ConstraintLayout constraintLayout;
+    private CustomTextView labelText;
+
+    @Mock
+    private CustomTextView numberText;
 
     @Before
     public void setUp() {
@@ -57,15 +63,24 @@ public class LabelFactoryTest extends BaseTest {
         FormUtils formUtilsSpy = Mockito.spy(formUtils);
         Assert.assertNotNull(formUtilsSpy);
 
+        Whitebox.setInternalState(factorySpy, "formUtils", formUtilsSpy);
+
         Mockito.doReturn(constraintLayout).when(formUtilsSpy).getRootConstraintLayout(context);
         Assert.assertNotNull(constraintLayout);
+
+        Mockito.doReturn(numberText).when(constraintLayout).findViewById(R.id.label_text_number);
+        Assert.assertNotNull(numberText);
 
         Mockito.doReturn(resources).when(context).getResources();
         Assert.assertNotNull(resources);
 
-        String labelString = "{\"key\":\"enabled_label\",\"type\":\"label\",\"text\":\"This is enabled\",\"hint_on_text\":false,\"text_color\":\"#FFC100\",\"text_size\":\"17sp\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"label_info_text\":\"Checking out the display oof the info icon needed to display this infomation\",\"label_info_title\":\"Highest Level of School\"}";
+        Mockito.doReturn(labelText).when(constraintLayout).findViewById(R.id.label_text);
+        Assert.assertNotNull(labelText);
 
-        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(labelString), listener, false);
+        Mockito.doReturn(0).when(factorySpy).getValueFromSpOrDpOrPx(ArgumentMatchers.eq(context), ArgumentMatchers.anyString());
+
+        String labelString = "{\"key\":\"enabled_label\",\"type\":\"label\",\"text\":\"This is enabled\",\"hint_on_text\":false,\"text_color\":\"#FFC100\",\"text_size\":\"17sp\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"label_info_text\":\"Checking out the display oof the info icon needed to display this infomation\",\"label_info_title\":\"Highest Level of School\",\"label_number\":\"1\",\"has_bg\":true,\"v_required\":{\"value\":true,\"err\":\"The required error\"}}";
+        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(labelString), listener);
         Assert.assertNotNull(viewList);
         Assert.assertTrue(viewList.size() > 0);
     }
