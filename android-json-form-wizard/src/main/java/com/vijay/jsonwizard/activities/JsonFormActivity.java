6512b1fccbc82e5c41bd3a5ddc17854de2aaa99b
Ephraim Kigamba <ekigamba@ona.io>
2020-05-07 11:07:59 +0300

    Code cleanup

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/JsonFormActivity.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/JsonFormActivity.java
@@ -14,8 +14,6 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.AppCompatRadioButton;
