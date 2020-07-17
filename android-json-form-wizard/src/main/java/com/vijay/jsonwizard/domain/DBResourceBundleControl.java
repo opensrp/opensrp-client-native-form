7151d892b4b1b86c45c71a57add5f294a33cc5e0
Allan O <a.onchuru@gmail.com>
2020-07-17 15:40:09 +0300

    :bug: Disable DBResouceBundle caching
    This is to allow handling different properties and also different locales

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/domain/DBResourceBundleControl.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/domain/DBResourceBundleControl.java
@@ -18,4 +18,8 @@ public class DBResourceBundleControl extends ResourceBundle.Control {
         return new DBResourceBundle(identifier);
     }
 
+    // Don't cache since the DBResourceBundle handles properties for different Locales
+    public long getTimeToLive(String arg0, Locale arg1) {
+        return TTL_DONT_CACHE;
+    }
 }
