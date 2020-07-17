de02abbdadb8a51946759eafcd573122285e7a4f
Allan O <a.onchuru@gmail.com>
2020-07-17 16:54:32 +0300

    :construction: Move get properties from repository to DBResourceBundleControl

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/domain/DBResourceBundle.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/domain/DBResourceBundle.java
@@ -1,31 +1,17 @@
 package com.vijay.jsonwizard.domain;
 
-import com.vijay.jsonwizard.utils.FormUtils;
-
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.ListResourceBundle;
 
 public class DBResourceBundle extends ListResourceBundle {
 
-    private String identifier;
+    private Object[][] properties;
 
-    public DBResourceBundle(String identifier) {
-        this.identifier = identifier;
+    public DBResourceBundle(Object[][] properties) {
+        this.properties = properties;
     }
 
     @Override
     protected Object[][] getContents() {
-        Object[][] properties = new Object[0][];
-        FormUtils formUtils = new FormUtils();
-        String propertiesString = formUtils.getPropertiesFileContentsFromDB(identifier);
-        if (StringUtils.isNotBlank(propertiesString)) {
-            String[] propertiesArray = propertiesString.split("\n");
-            properties = new Object[propertiesArray.length][];
-            for (int i = 0; i < propertiesArray.length; i++) {
-                properties[i] = propertiesArray[i].trim().split("\\s*=\\s*");
-            }
-        }
-        return properties;
+        return this.properties;
     }
 }
--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/domain/DBResourceBundleControl.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/domain/DBResourceBundleControl.java
@@ -1,5 +1,9 @@
 package com.vijay.jsonwizard.domain;
 
+import com.vijay.jsonwizard.utils.FormUtils;
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -15,11 +19,28 @@ public class DBResourceBundleControl extends ResourceBundle.Control {
     public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
             throws IllegalAccessException,
             InstantiationException {
-        return new DBResourceBundle(identifier);
+        return new DBResourceBundle(getPropertiesFromRepository(identifier));
     }
 
     // Don't cache since the DBResourceBundle handles properties for different Locales
     public long getTimeToLive(String arg0, Locale arg1) {
         return TTL_DONT_CACHE;
+    }
+
+    private Object[][] getPropertiesFromRepository(String identifier) {
+        Object[][] properties = new Object[0][];
+        if (StringUtils.isNotBlank(this.identifier)) {
+            FormUtils formUtils = new FormUtils();
+            String propertiesString = formUtils.getPropertiesFileContentsFromDB(identifier);
+            if (StringUtils.isNotBlank(propertiesString)) {
+                String[] propertiesArray = propertiesString.split("\n");
+                properties = new Object[propertiesArray.length][];
+                for (int i = 0; i < propertiesArray.length; i++) {
+                    properties[i] = propertiesArray[i].trim().split("\\s*=\\s*");
+                }
+            }
+
+        }
+        return properties;
     }
 }
