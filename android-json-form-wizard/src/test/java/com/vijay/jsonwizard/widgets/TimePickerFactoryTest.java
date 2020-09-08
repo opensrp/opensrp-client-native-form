1622b9cd39183c3088999bf463c93b79bfafe8da
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-09-08 11:53:11 +0300

    :ok_hand: Adding unit tests for the date picker & time picker factory

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/TimePickerFactoryTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/TimePickerFactoryTest.java
@@ -1,10 +1,8 @@
 package com.vijay.jsonwizard.widgets;
 
-import android.content.res.Resources;
 import android.text.Editable;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.vijay.jsonwizard.BaseTest;
@@ -17,75 +15,64 @@ import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
+import org.robolectric.Robolectric;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 public class TimePickerFactoryTest extends BaseTest {
     private TimePickerFactory factory;
-    @Mock
-    private JsonFormActivity context;
+    private JsonFormActivity jsonFormActivity;
 
     @Mock
     private JsonFormFragment formFragment;
-
-    @Mock
-    private Resources resources;
-
     @Mock
     private CommonListener listener;
-
     @Mock
-    private RelativeLayout rootLayout;
-
-    @Mock
-    private MaterialEditText editText;
-
-    @Mock
-    private TextView duration;
+    private View view;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         factory = new TimePickerFactory();
+        jsonFormActivity = Robolectric.buildActivity(JsonFormActivity.class, getJsonFormActivityIntent()).create().get();
     }
 
     @Test
     public void testTimePickerFactoryInstantiatesViewsCorrectly() throws Exception {
-        Assert.assertNotNull(factory);
-        TimePickerFactory factorySpy = Mockito.spy(factory);
-
-        Mockito.doReturn(resources).when(context).getResources();
-        Assert.assertNotNull(resources);
-
-        context.setTheme(R.style.NativeFormsAppTheme);
-        Mockito.doReturn(rootLayout).when(factorySpy).getRelativeLayout(context);
-        Mockito.doReturn(editText).when(rootLayout).findViewById(R.id.edit_text);
-        Mockito.doReturn(duration).when(rootLayout).findViewById(R.id.duration);
-        Mockito.doReturn(Locale.ENGLISH).when(factorySpy).getCurrentLocale(context);
-        Mockito.doReturn("hrs").when(duration).getTag(ArgumentMatchers.anyInt());
-
         String timePickerWidget = "{\"key\":\"user_time\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"type\":\"time_picker\",\"hint\":\"Birth Time\",\"expanded\":false,\"duration\":{\"label\":\"Birth Time\"},\"v_required\":{\"value\":true,\"err\":\"Please enter the time of birth\"},\"relevance\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"sample-relevance-rules.yml\"}}},\"constraints\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"sample-constraints-rules.yml\"}}},\"calculation\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"sample-calculation-rules.yml\"}}},\"value\":\"22:03\",\"read_only\":true,\"label_info_text\":\"Just testing\",\"label_info_title\":\"Just testing\"}";
-        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(timePickerWidget), listener);
+        List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, new JSONObject(timePickerWidget), listener);
         Assert.assertNotNull(viewList);
         Assert.assertEquals(1, viewList.size());
+
+        View rootLayout = viewList.get(0);
+        Assert.assertEquals(2, ((RelativeLayout) rootLayout).getChildCount());
+
+        MaterialEditText materialEditText = (MaterialEditText) ((RelativeLayout) rootLayout).getChildAt(0);
+
+        Assert.assertEquals("user_time", materialEditText.getTag(R.id.key));
+        Assert.assertEquals("", materialEditText.getTag(R.id.openmrs_entity_parent));
+        Assert.assertEquals("", materialEditText.getTag(R.id.openmrs_entity));
+        Assert.assertEquals("", materialEditText.getTag(R.id.openmrs_entity_id));
     }
 
     @Test
     public void testUpdateTimeText() throws Exception {
-        Assert.assertNotNull(factory);
-        TimePickerFactory factorySpy = Mockito.spy(factory);
-        Mockito.doReturn(rootLayout).when(factorySpy).getRelativeLayout(context);
-        Mockito.doReturn(editText).when(rootLayout).findViewById(R.id.edit_text);
+        String timePickerWidget = "{\"key\":\"user_time\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"type\":\"time_picker\",\"hint\":\"Birth Time\",\"expanded\":false,\"duration\":{\"label\":\"Birth Time\"},\"v_required\":{\"value\":true,\"err\":\"Please enter the time of birth\"},\"relevance\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"sample-relevance-rules.yml\"}}},\"constraints\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"sample-constraints-rules.yml\"}}},\"calculation\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"sample-calculation-rules.yml\"}}},\"value\":\"22:03\",\"read_only\":true,\"label_info_text\":\"Just testing\",\"label_info_title\":\"Just testing\"}";
+        List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, new JSONObject(timePickerWidget), listener);
+        Assert.assertNotNull(viewList);
+
+        View rootLayout = viewList.get(0);
+        Assert.assertEquals(2, ((RelativeLayout) rootLayout).getChildCount());
+
+        MaterialEditText materialEditText = (MaterialEditText) ((RelativeLayout) rootLayout).getChildAt(0);
+        Mockito.doReturn(jsonFormActivity).when(formFragment).getContext();
         Editable editable = new Editable.Factory().newEditable("23:03");
-        Whitebox.invokeMethod(factorySpy, "updateTimeText", editText, 22, 3);
-        Mockito.doReturn(editable).when(editText).getText();
+        Whitebox.invokeMethod(factory, "updateTimeText", materialEditText, 22, 3);
         Assert.assertEquals("23:03", editable.toString());
 
     }
