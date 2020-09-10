f32f8705793a7828c99ee5fda8ff3cb6984d85fb
Vincent Karuri <vincentmkaruri@gmail.com>
2020-09-10 12:59:11 +0300

    Revert "Add RepeatingGroupFactory unique id generation test"
    This reverts commit bdbc3f8f435ea642f52bdaec5ce45e2be5c79007.

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/RepeatingGroupFactoryTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/RepeatingGroupFactoryTest.java
@@ -1,28 +1,21 @@
 package com.vijay.jsonwizard.widgets;
 
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.vijay.jsonwizard.R;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
-import com.vijay.jsonwizard.domain.WidgetArgs;
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.CommonListener;
-import com.vijay.jsonwizard.task.AttachRepeatingGroupTask;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.robolectric.util.ReflectionHelpers;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -60,32 +53,6 @@ public class RepeatingGroupFactoryTest extends FactoryTest {
 
         Set<String> editableProperties = factorySpy.getCustomTranslatableWidgetFields();
         Assert.assertEquals(0, editableProperties.size());
-    }
-
-    @Test
-    public void testUniqueChildElementKeyGenerationShouldContainParentAndChildKeyAsComponents() throws JSONException {
-        final String parentRepeatingGroupWidgetKey = "parent";
-        final String childElementKey = "child";
-        final String uniqueId = "unique";
-
-        JSONObject parentRepeatingGroupWidget = new JSONObject();
-        parentRepeatingGroupWidget.put(JsonFormConstants.KEY, parentRepeatingGroupWidgetKey);
-        WidgetArgs widgetArgs = new WidgetArgs().withJsonObject(parentRepeatingGroupWidget);
-
-        JSONObject childElement = new JSONObject();
-        childElement.put(JsonFormConstants.KEY, childElementKey);
-
-        AttachRepeatingGroupTask attachRepeatingGroupTask = new AttachRepeatingGroupTask(Mockito.mock(LinearLayout.class),
-                0, new HashMap<Integer, String>(), widgetArgs, Mockito.mock(ImageButton.class));
-
-        ReflectionHelpers.callInstanceMethod(attachRepeatingGroupTask,
-                "addUniqueIdentifiers",
-                ReflectionHelpers.ClassParameter.from(JSONObject.class, childElement),
-                ReflectionHelpers.ClassParameter.from(String.class, uniqueId));
-
-        String[] uniqueIdComponents = childElement.getString(JsonFormConstants.KEY).split("_");
-        Assert.assertEquals(parentRepeatingGroupWidgetKey, uniqueIdComponents[0]);
-        Assert.assertEquals(childElementKey, uniqueIdComponents[1]);
     }
 
     private List<View> invokeGetViewsFromJson() throws Exception {
