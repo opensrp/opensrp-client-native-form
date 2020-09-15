ec19eb5eee7386bf16652356141be8af3219e817
Samuel Githengi <samuel.githengi@gmail.com>
2020-09-15 11:08:46 +0300

    Unit test click opens simprints

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/presenters/JsonWizardFormFragmentPresenterRoboelectricTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/presenters/JsonWizardFormFragmentPresenterRoboelectricTest.java
@@ -1,8 +1,13 @@
 package com.vijay.jsonwizard.presenters;
 
+import android.content.Context;
+import android.view.View;
+
 import com.vijay.jsonwizard.BaseTest;
+import com.vijay.jsonwizard.R;
 import com.vijay.jsonwizard.TestConstants;
 import com.vijay.jsonwizard.activities.JsonFormActivity;
+import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.fragments.JsonWizardFormFragment;
 import com.vijay.jsonwizard.interactors.JsonFormInteractor;
 import com.vijay.jsonwizard.interfaces.OnFieldsInvalid;
@@ -12,6 +17,9 @@ import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.robolectric.RuntimeEnvironment;
+
+import java.util.UUID;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,6 +43,8 @@ public class JsonWizardFormFragmentPresenterRoboelectricTest extends BaseTest {
 
     private JsonWizardFormFragmentPresenter formFragmentPresenter;
 
+    private Context context = RuntimeEnvironment.application;
+
     @Before
     public void setUp() throws JSONException {
         when(formFragment.getJsonApi()).thenReturn(formActivity);
@@ -53,5 +63,31 @@ public class JsonWizardFormFragmentPresenterRoboelectricTest extends BaseTest {
         verify(formActivity).initializeDependencyMaps();
         verify(formActivity).setNextStepRelevant(false);
         verify(formFragment).skipStepsOnNextPressed("step2");
+    }
+
+    @Test
+    public void testOnClickShouldOpenSimPrintsRegistration() {
+        View view = new View(context);
+        view.setTag(R.id.type, JsonFormConstants.FINGER_PRINT);
+        view.setTag(R.id.finger_print_option, JsonFormConstants.SIMPRINTS_OPTION_REGISTER);
+        view.setTag(R.id.project_id, "test");
+        view.setTag(R.id.module_id, "nf");
+        view.setTag(R.id.user_id, "jdoe");
+        formFragmentPresenter.onClick(view);
+        verify(formFragment).startSimprintsRegistration("test", "jdoe", "nf");
+    }
+
+    @Test
+    public void testOnClickShouldOpenSimPrintsVerification() {
+        View view = new View(context);
+        view.setTag(R.id.type, JsonFormConstants.FINGER_PRINT);
+        view.setTag(R.id.finger_print_option, JsonFormConstants.SIMPRINTS_OPTION_VERIFY);
+        view.setTag(R.id.project_id, "test");
+        view.setTag(R.id.module_id, "nf");
+        view.setTag(R.id.user_id, "jdoe");
+        String guid = UUID.randomUUID().toString();
+        view.setTag(R.id.guid, guid);
+        formFragmentPresenter.onClick(view);
+        verify(formFragment).startSimprintsVerification("test", "jdoe", "nf", guid);
     }
 }
