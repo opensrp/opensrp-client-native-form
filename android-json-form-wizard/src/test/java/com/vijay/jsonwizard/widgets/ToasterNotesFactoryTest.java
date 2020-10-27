00eaa536e465cd511f0b9b2e9f2bae3fdb1a4639
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-10-27 11:32:28 +0300

    :ok_hand: add toaster notes tests

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/ToasterNotesFactoryTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/ToasterNotesFactoryTest.java
@@ -1,15 +1,9 @@
 package com.vijay.jsonwizard.widgets;
 
-import android.content.res.Resources;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.vijay.jsonwizard.BaseTest;
-import com.vijay.jsonwizard.R;
 import com.vijay.jsonwizard.activities.JsonFormActivity;
-import com.vijay.jsonwizard.customviews.ToasterLinearLayout;
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.CommonListener;
 
@@ -18,164 +12,66 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
 
 import java.util.List;
+import java.util.Set;
 
 public class ToasterNotesFactoryTest extends BaseTest {
-
     private ToasterNotesFactory factory;
-    @Mock
-    private JsonFormActivity context;
+    private JsonFormActivity jsonFormActivity;
 
     @Mock
     private JsonFormFragment formFragment;
-
     @Mock
     private CommonListener listener;
-
-    @Mock
-    private ToasterLinearLayout rootLayout;
-
-    @Mock
-    private Resources resources;
-
-    @Mock
-    private RelativeLayout toasterRelativeLayout;
-
-    @Mock
-    private ImageView toasterNoteImageView;
-
-    @Mock
-    private ImageView toasterNoteInfo;
-
-    @Mock
-    private TextView toasterNotesTextView;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         factory = new ToasterNotesFactory();
+        jsonFormActivity = Robolectric.buildActivity(JsonFormActivity.class, getJsonFormActivityIntent()).create().get();
     }
 
     @Test
     public void testInfoToasterInstantiatesViewsCorrectly() throws Exception {
-        Assert.assertNotNull(factory);
-        ToasterNotesFactory factorySpy = Mockito.spy(factory);
-        Assert.assertNotNull(factorySpy);
-
-        Mockito.doReturn(rootLayout).when(factorySpy).getToasterLinearLayout(context);
-        Assert.assertNotNull(rootLayout);
-
-        Mockito.doReturn(toasterRelativeLayout).when(rootLayout).findViewById(R.id.toaster_notes_layout);
-        Assert.assertNotNull(toasterRelativeLayout);
-
-        Mockito.doReturn(resources).when(context).getResources();
-        Assert.assertNotNull(resources);
-
-        Mockito.doReturn(toasterNoteImageView).when(rootLayout).findViewById(R.id.toaster_notes_image);
-        Assert.assertNotNull(toasterNoteImageView);
-
-        Mockito.doReturn(toasterNoteInfo).when(rootLayout).findViewById(R.id.toaster_notes_info);
-        Assert.assertNotNull(toasterNoteInfo);
-
-        Mockito.doReturn(toasterNotesTextView).when(rootLayout).findViewById(R.id.toaster_notes_text);
-        Assert.assertNotNull(toasterNotesTextView);
-
         String labelString = "{\"key\":\"ultrasound_info_toaster\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"type\":\"toaster_notes\",\"text\":\"Test toaster notes\",\"toaster_type\":\"info\",\"relevance\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_relevance_rules.yml\"}}},\"calculation\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_calculation_rules.yml\"}}},\"constraints\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_constraints_rules.yml\"}}}}";
-        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(labelString), listener);
+        List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, new JSONObject(labelString), listener);
         Assert.assertNotNull(viewList);
-        Assert.assertEquals(1,viewList.size());
+        Assert.assertEquals(1, viewList.size());
     }
 
     @Test
     public void testWarningToasterInstantiatesViewsCorrectly() throws Exception {
-        Assert.assertNotNull(factory);
-        ToasterNotesFactory factorySpy = Mockito.spy(factory);
-        Assert.assertNotNull(factorySpy);
-
-        Mockito.doReturn(rootLayout).when(factorySpy).getToasterLinearLayout(context);
-        Assert.assertNotNull(rootLayout);
-
-        Mockito.doReturn(toasterRelativeLayout).when(rootLayout).findViewById(R.id.toaster_notes_layout);
-        Assert.assertNotNull(toasterRelativeLayout);
-
-        Mockito.doReturn(resources).when(context).getResources();
-        Assert.assertNotNull(resources);
-
-        Mockito.doReturn(toasterNoteImageView).when(rootLayout).findViewById(R.id.toaster_notes_image);
-        Assert.assertNotNull(toasterNoteImageView);
-
-        Mockito.doReturn(toasterNoteInfo).when(rootLayout).findViewById(R.id.toaster_notes_info);
-        Assert.assertNotNull(toasterNoteInfo);
-
-        Mockito.doReturn(toasterNotesTextView).when(rootLayout).findViewById(R.id.toaster_notes_text);
-        Assert.assertNotNull(toasterNotesTextView);
 
         String labelString = "{\"key\":\"ultrasound_info_toaster\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"type\":\"toaster_notes\",\"text\":\"Test toaster notes\",\"toaster_type\":\"warning\",\"relevance\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_relevance_rules.yml\"}}},\"calculation\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_calculation_rules.yml\"}}},\"constraints\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_constraints_rules.yml\"}}}}";
-        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(labelString), listener);
+        List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, new JSONObject(labelString), listener);
         Assert.assertNotNull(viewList);
-        Assert.assertEquals(1,viewList.size());
+        Assert.assertEquals(1, viewList.size());
     }
 
     @Test
     public void testProblemToasterInstantiatesViewsCorrectly() throws Exception {
-        Assert.assertNotNull(factory);
-        ToasterNotesFactory factorySpy = Mockito.spy(factory);
-        Assert.assertNotNull(factorySpy);
-
-        Mockito.doReturn(rootLayout).when(factorySpy).getToasterLinearLayout(context);
-        Assert.assertNotNull(rootLayout);
-
-        Mockito.doReturn(toasterRelativeLayout).when(rootLayout).findViewById(R.id.toaster_notes_layout);
-        Assert.assertNotNull(toasterRelativeLayout);
-
-        Mockito.doReturn(resources).when(context).getResources();
-        Assert.assertNotNull(resources);
-
-        Mockito.doReturn(toasterNoteImageView).when(rootLayout).findViewById(R.id.toaster_notes_image);
-        Assert.assertNotNull(toasterNoteImageView);
-
-        Mockito.doReturn(toasterNoteInfo).when(rootLayout).findViewById(R.id.toaster_notes_info);
-        Assert.assertNotNull(toasterNoteInfo);
-
-        Mockito.doReturn(toasterNotesTextView).when(rootLayout).findViewById(R.id.toaster_notes_text);
-        Assert.assertNotNull(toasterNotesTextView);
 
         String labelString = "{\"key\":\"ultrasound_info_toaster\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"type\":\"toaster_notes\",\"text\":\"Test toaster notes\",\"toaster_type\":\"problem\",\"toaster_info_text\":\"Toaster text\",\"toaster_info_title\":\"Toaster titles\",\"relevance\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_relevance_rules.yml\"}}},\"calculation\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_calculation_rules.yml\"}}},\"constraints\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_constraints_rules.yml\"}}}}";
-        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(labelString), listener);
+        List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, new JSONObject(labelString), listener);
         Assert.assertNotNull(viewList);
-        Assert.assertEquals(1,viewList.size());
+        Assert.assertEquals(1, viewList.size());
     }
 
     @Test
     public void testPositiveToasterInstantiatesViewsCorrectly() throws Exception {
-        Assert.assertNotNull(factory);
-        ToasterNotesFactory factorySpy = Mockito.spy(factory);
-        Assert.assertNotNull(factorySpy);
-
-        Mockito.doReturn(rootLayout).when(factorySpy).getToasterLinearLayout(context);
-        Assert.assertNotNull(rootLayout);
-
-        Mockito.doReturn(toasterRelativeLayout).when(rootLayout).findViewById(R.id.toaster_notes_layout);
-        Assert.assertNotNull(toasterRelativeLayout);
-
-        Mockito.doReturn(resources).when(context).getResources();
-        Assert.assertNotNull(resources);
-
-        Mockito.doReturn(toasterNoteImageView).when(rootLayout).findViewById(R.id.toaster_notes_image);
-        Assert.assertNotNull(toasterNoteImageView);
-
-        Mockito.doReturn(toasterNoteInfo).when(rootLayout).findViewById(R.id.toaster_notes_info);
-        Assert.assertNotNull(toasterNoteInfo);
-
-        Mockito.doReturn(toasterNotesTextView).when(rootLayout).findViewById(R.id.toaster_notes_text);
-        Assert.assertNotNull(toasterNotesTextView);
-
         String labelString = "{\"key\":\"ultrasound_info_toaster\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"\",\"openmrs_entity_id\":\"\",\"type\":\"toaster_notes\",\"text\":\"Test toaster notes\",\"toaster_type\":\"positive\",\"relevance\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_relevance_rules.yml\"}}},\"calculation\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_calculation_rules.yml\"}}},\"constraints\":{\"rules-engine\":{\"ex-rules\":{\"rules-file\":\"ultrasound_sub_form_constraints_rules.yml\"}}}}";
-        List<View> viewList = factorySpy.getViewsFromJson("RandomStepName", context, formFragment, new JSONObject(labelString), listener);
+        List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, new JSONObject(labelString), listener);
         Assert.assertNotNull(viewList);
-        Assert.assertEquals(1,viewList.size());
+        Assert.assertEquals(1, viewList.size());
+    }
+
+    @Test
+    public void testGetCustomTranslatableWidgetFields() {
+        Set<String> editableProperties = factory.getCustomTranslatableWidgetFields();
+        Assert.assertEquals(2, editableProperties.size());
+        Assert.assertEquals("toaster_info_text", editableProperties.iterator().next());
     }
 }
