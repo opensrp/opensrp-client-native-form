b6011a3a2d1688c538e85bb3ef59161272652b15
Qazi Abubakar <qma_2007@yahoo.com>
2020-10-29 14:41:02 +0500

    Added new test case

---
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/SectionFactoryTest.java
@@ -0,0 +1,31 @@
+package com.vijay.jsonwizard.widgets;
+
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SectionFactoryTest extends FactoryTest {
+
+    private SectionFactory factory;
+    private JSONObject step;
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+        factory = new SectionFactory();
+    }
+
+    @Test
+    public void testGetCustomTranslatableWidgetFields() {
+        SectionFactory factorySpy = Mockito.spy(factory);
+
+        Set<String> editableProperty = factorySpy.getCustomTranslatableWidgetFields();
+        Assert.assertEquals(new HashSet<>(), editableProperty);
+    }
+}
