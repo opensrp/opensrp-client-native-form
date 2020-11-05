279494136ee63249c58d64d882a56447535023d5
Qazi Abubakar <qma_2007@yahoo.com>
2020-11-05 13:26:20 +0500

    code refactoring

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/RepeatingGroupFactory.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/widgets/RepeatingGroupFactory.java
@@ -351,10 +351,11 @@ public class RepeatingGroupFactory implements FormWidgetFactory {
                 doneButton.setImageResource(R.drawable.ic_done_grey);
                 ValidationStatus validationStatus = JsonFormFragmentPresenter
                         .validate(widgetArgs.getFormFragment(), referenceEditText, false);
-                if (widgetArgs.getJsonObject().optBoolean(JsonFormConstants.EXPAND_ON_TEXT_CHANGE) && validationStatus.isValid())
-                    addOnDoneAction(referenceEditText, doneButton, widgetArgs);
                 if (validationStatus.isValid()) {
                     writeJsonObjectValue(repeatingGroupCount, s.toString());
+                    if (widgetArgs.getJsonObject().optBoolean(JsonFormConstants.EXPAND_ON_TEXT_CHANGE)) {
+                        addOnDoneAction(referenceEditText, doneButton, widgetArgs);
+                    }
                 }
             }
         });
