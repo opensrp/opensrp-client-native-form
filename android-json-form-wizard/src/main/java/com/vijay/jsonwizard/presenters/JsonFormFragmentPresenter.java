7042808057053d47432604c56b2a981cc9b90322
zeeshan <zeeshan.afzal@venturedive.com>
2020-11-20 17:55:33 +0500

    unit test added for Utils.removeDeletedInvalidFields

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/presenters/JsonFormFragmentPresenter.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/presenters/JsonFormFragmentPresenter.java
@@ -71,7 +71,6 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
