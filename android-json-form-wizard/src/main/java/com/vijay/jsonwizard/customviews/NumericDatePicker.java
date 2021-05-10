27a89286194652f5f41b8352b72b5556eb4a94e2
Simon Njoroge <snjoroge@ona.io>
2021-05-10 10:34:44 +0300

    Fix codacy issue

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/customviews/NumericDatePicker.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/customviews/NumericDatePicker.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 
 import com.vijay.jsonwizard.R;
-import com.vijay.jsonwizard.utils.DateUtil;
 import com.vijay.jsonwizard.utils.NumericDatePickerHelper;
 import com.vijay.jsonwizard.utils.NumericDatePickerValidator;
 
