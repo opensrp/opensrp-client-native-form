0551aeb14ba3e3ab5807a1c201f2b5eee8dd19d2
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2021-05-24 18:25:34 +0500

    Fix failing tests

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/NativeRadioButtonFactoryTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/widgets/NativeRadioButtonFactoryTest.java
@@ -50,6 +50,9 @@ public class NativeRadioButtonFactoryTest extends BaseTest {
         Assert.assertNotNull(formUtilsSpy);
         Whitebox.setInternalState(factory, "formUtils", formUtilsSpy);
 
+        //noinspection ResultOfMethodCallIgnored
+        Mockito.doReturn(jsonFormActivity).when(formFragment).getJsonApi();
+
         List<View> viewList = factory.getViewsFromJson("RandomStepName", jsonFormActivity, formFragment, nativeRadioButtonObject, listener);
         Assert.assertNotNull(viewList);
         Assert.assertTrue(viewList.size() > 0);
