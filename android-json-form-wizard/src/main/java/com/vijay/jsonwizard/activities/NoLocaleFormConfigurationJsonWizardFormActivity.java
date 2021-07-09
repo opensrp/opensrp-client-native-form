a787630deed303eae20b45920c2da1470b09fc9f
LZRS <lazloti@gmail.com>
2021-07-09 16:02:41 +0300

    refactor name isUsingLocaleForms to supportsLocaleBasedForms
    and other formatting improvements

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/NoLocaleFormConfigurationJsonWizardFormActivity.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/NoLocaleFormConfigurationJsonWizardFormActivity.java
@@ -2,7 +2,7 @@ package com.vijay.jsonwizard.activities;
 
 public class NoLocaleFormConfigurationJsonWizardFormActivity extends FormConfigurationJsonWizardFormActivity {
     @Override
-    protected boolean isUsingLocaleForms() {
+    protected boolean supportsLocaleBasedForms() {
         return false;
     }
 }
