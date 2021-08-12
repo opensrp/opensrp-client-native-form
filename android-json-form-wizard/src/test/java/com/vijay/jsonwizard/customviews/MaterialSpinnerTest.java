6e3b7b16417c0d4f81921a8d1797569953663cac
AbdulWahab <abdulwahab.memon@venturedive.com>
2021-08-12 11:37:48 +0500

    AWM: #121 codacy fix remove unused import

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/customviews/MaterialSpinnerTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/customviews/MaterialSpinnerTest.java
@@ -3,7 +3,6 @@ package com.vijay.jsonwizard.customviews;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 
