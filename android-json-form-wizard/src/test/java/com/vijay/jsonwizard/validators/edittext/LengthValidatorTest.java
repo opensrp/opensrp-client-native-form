7458197ee64efbcc882f0e17f489ba0d9ca0d277
Simon Njoroge <snjoroge@ona.io>
2022-05-14 01:05:32 +0300

    Fix unit tests

--- android-json-form-wizard/src/test/java/com/vijay/jsonwizard/validators/edittext/LengthValidatorTest.java
+++ android-json-form-wizard/src/test/java/com/vijay/jsonwizard/validators/edittext/LengthValidatorTest.java
@@ -8,18 +8,26 @@ import org.junit.Test;
 public class LengthValidatorTest extends BaseTest {
 
     @Test
-    public void isValidShouldReturnFalseWhenIsEmptyTrue() {
-
+    public void isValidShouldReturnTrueWhenEditTextValueIsEmptyAndMinimumLengthIsZero() {
         LengthValidator validator = new LengthValidator(DEFAULT_ERROR_MSG, CONSTANT_INT_0, CONSTANT_INT_50);
-        Assert.assertEquals(false, validator.isValid(DEFAULT_TEST_MESSAGE, true));
-
+        Assert.assertEquals(true, validator.isValid("", true));
     }
 
     @Test
-    public void isValidShouldReturnTrueWhenIsEmptyFalse() {
+    public void isValidShouldReturnTrueWhenEditTextValueIsNotEmptyAndIsLowerThanMaximumLength() {
+        LengthValidator validator = new LengthValidator(DEFAULT_ERROR_MSG, CONSTANT_INT_0, CONSTANT_INT_50);
+        Assert.assertEquals(true, validator.isValid(DEFAULT_TEST_MESSAGE, false));
+    }
 
+    @Test
+    public void isValidShouldReturnFalseWhenEditTextValueIsLongerThanMaximumLength() {
+        LengthValidator validator = new LengthValidator(DEFAULT_ERROR_MSG, CONSTANT_INT_0, 20);
+        Assert.assertEquals(false, validator.isValid("This message is longer", false));
+    }
+
+    @Test
+    public void isValidShouldReturnTrueWhenEditTextValueIsLongerThanMinimumLengthAndShorterThanMaximumLength() {
         LengthValidator validator = new LengthValidator(DEFAULT_ERROR_MSG, CONSTANT_INT_0, CONSTANT_INT_50);
         Assert.assertEquals(true, validator.isValid(DEFAULT_TEST_MESSAGE, false));
     }
 }
-
